c8e1adf7f6eb7be7447a7db66ce65993bcc62e0e
firouni <firas.el.ouni@gmail.com>
2023-01-18 18:01:40 +0100

    fiir

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,14 @@
-example
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+    res.write('<h1>Hello Node!!!</h1>');
+    res.end();
+});
+
+server.listen(3000, (err) => {
+    if(err){
+        console.log(err)
+    } else {
+        console.log('The Server is Running on port 3000');
+    }
+});
